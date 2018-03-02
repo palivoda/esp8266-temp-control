12d6bd841c96db8fb7e4927d226833a985ca4383
Rostislav Palivoda <rostislav.palivoda@gmail.com>
2018-03-02 13:01:20 +0200

    - draft logic for valve and motor

--- src/hardware/HardwareControllerPoC1.cpp
+++ src/hardware/HardwareControllerPoC1.cpp
@@ -1,6 +1,8 @@
 
 #include "HardwareControllerPoC1.h"
 
+static HardwareControllerPoC1 HW;
+
 void HardwareControllerPoC1::init() {
   pinMode(PIN_MOTOR, OUTPUT);
   digitalWrite(PIN_MOTOR, LOW);
@@ -17,39 +19,61 @@ void HardwareControllerPoC1::loop() {
   motorTimer.update();
 }
 
-void HardwareControllerPoC1::openValve(short s) {
-  valveTimer.pulseImmediate(PIN_VALVE_OPEN, s, HIGH); //TODO: callback to change valve to stopped state
-  digitalWrite(PIN_VALVE_CLOSE, LOW);
-  valve = VALVE_OPENING;
+void HardwareControllerPoC1::openValve(long ms) {
+  if (valveTimer.after(ms, &stopValve) == -1) {
+    Serial.print("OpenValve timer setup failed");
+  }
+  else {
+    digitalWrite(PIN_VALVE_OPEN, HIGH);
+    digitalWrite(PIN_VALVE_CLOSE, LOW);
+    valveAction = VALVE_OPENING;
+    ValveState += ms;
+  }
 }
 
-void HardwareControllerPoC1::closeValve(short s) {
-  digitalWrite(PIN_VALVE_OPEN, LOW);
-  valveTimer.pulseImmediate(PIN_VALVE_CLOSE, s, HIGH);  //TODO: callback to change valve to stopped state
-  valve = VALVE_CLOSING;
+void HardwareControllerPoC1::closeValve(long ms) {
+  if (valveTimer.after(ms, &stopValve) == -1) {
+    Serial.print("OpenValve timer setup failed");
+  }
+  else {
+    digitalWrite(PIN_VALVE_OPEN, LOW);
+    digitalWrite(PIN_VALVE_CLOSE, HIGH);
+    valveAction = VALVE_CLOSING;
+    ValveState -= ms;
+  }
 }
 
 void HardwareControllerPoC1::stopValve() {
   digitalWrite(PIN_VALVE_OPEN, LOW);
   digitalWrite(PIN_VALVE_CLOSE, LOW);
-  valve = VALVE_STOPPED;
+  HW.valveAction = VALVE_STOPPED;
 }
 
-VALVE_STATE HardwareControllerPoC1::readValve() {
-  return valve;
+VALVE_STATE HardwareControllerPoC1::readValveAction() {
+  return valveAction;
 }
 
-void HardwareControllerPoC1::turnMotor(bool on) {  //TODO: add period, optional, use timer
-  motor = on;
-  digitalWrite(PIN_MOTOR, on ? HIGH : LOW);
+void HardwareControllerPoC1::startMotor(long ms) {
+  if (valveTimer.after(ms, &stopValve) == -1) {
+    Serial.print("Start Motor timer setup failed");
+  }
+  else {
+    digitalWrite(PIN_MOTOR, HIGH);
+    motorActive = true;
+  }
+}
+
+void HardwareControllerPoC1::stopMotor() {
+  HW.motorActive = false;
+  digitalWrite(PIN_MOTOR, LOW);
 }
 
 bool HardwareControllerPoC1::readMotor() {
-    return motor;
+    return motorActive;
 }
 
-float HardwareControllerPoC1::readMotorCurrent() {
-    return 0.0;
+float HardwareControllerPoC1::readMotorAmps() {
+    return _ACS712(PIN_AMPS);
 }
 
 float HardwareControllerPoC1::readFeedTemp()
@@ -60,4 +84,23 @@ float HardwareControllerPoC1::readFeedTemp()
 float HardwareControllerPoC1::readReturnTemp()
 {
   return returnTemp.getTemp();
+}
+
+/*
+  Returns  current consumption reading from ACS712 sensor.
+  Motor max I (amps) = U (volts) / R (ohms), where U=12V and R is resistance of motor coil.
+  Feeder - 10A max
+  Fan 4mA - max
+ */
+float HardwareControllerPoC1::_ACS712(byte pin) {
+  const byte mVperAmp = 66; // use 185 for 10A, 100 for 20A Module and 66 for 30A Module
+  const short ACSoffset = 2485; //milivolts
+
+  short rawVal = analogRead(pin);
+  float mVolts = ((rawVal+1) / 1024.0) * 5000;
+  float amps = ((mVolts - ACSoffset) / mVperAmp);
+
+  //if (amps > 4) Serial.println(">>>>>>>>>>> HIGH!!!!");
+  //Serial.println(String("ACS712 ") + pin + "=" + rawVal + " => " + mVolts + "mV, " + amps  + "A");
+  return amps;
 }
--- src/hardware/HardwareControllerPoC1.h
+++ src/hardware/HardwareControllerPoC1.h
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <Timer.h>
 #include "NtcMf52at10k.h"
 
 #ifndef HardwareControllerPoC1_hpp
@@ -7,6 +8,7 @@
 #define PIN_MOTOR D0
 #define PIN_VALVE_OPEN D5
 #define PIN_VALVE_CLOSE D6
+#define PIN_AMPS D7
 #define PIN_TEMP_FEED 1
 #define PIN_TEMP_RETURN 2
 
@@ -20,25 +22,30 @@ class HardwareControllerPoC1 {
 
   private:
 
-    bool motor;
-    VALVE_STATE valve;
+    bool motorActive;
+    VALVE_STATE valveAction = VALVE_STOPPED;
+    long ValveState = 0;
     NtcMf52at10k feedTemp = NtcMf52at10k(PIN_TEMP_FEED);
     NtcMf52at10k returnTemp = NtcMf52at10k(PIN_TEMP_RETURN);
+    Timer valveTimer;
+    Timer motorTimer;
+    float _ACS712(byte pin);
 
    public:
 
      void init();
      void loop();
 
-     short Valve ;
-     void openValve(short s);
-     void closeValve(short s);
-     void stopValve();
-     VALVE_STATE readValve();
+     long ValveConfig = 120000;
+     void openValve(long ms);
+     void closeValve(long ms);
+     static void stopValve();
+     VALVE_STATE readValveAction();
 
-     void turnMotor(bool on);
+     void startMotor(long ms);
+     static void stopMotor();
      bool readMotor();
-     float readMotorCurrent();
+     float readMotorAmps();
 
      float readFeedTemp();
      float readReturnTemp();
